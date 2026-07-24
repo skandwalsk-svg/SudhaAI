02a5879da80a53d064d9f37a15515795cc6cd179
skandwalsk-svg <s.kandwal.sk@gmail.com>
2026-07-24 10:11:28 +0530

    Delete src/src directory

--- src/src/App.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: '20px', textAlign: 'center', fontFamily: 'sans-serif' }}>
-      <h1>Sudha AI is Live!</h1>
-      <p>App component is working successfully.</p>
-    </div>
-  )
-}
